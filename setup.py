03ccb1ca4d99356d63a26ccc6ae9c40a5afba217
Prabesh1sharma <sharmaprabesh2027@gmail.com>
2025-02-04 11:25:34 +0545

    project structure ready and env setup

---
+++ setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup, find_packages
+
+setup(
+    name = "src",
+    version="0.0.1",
+    author="Prabesh",
+    author_email="sharmaprabesh2027@gmail.com",
+    packages=find_packages()
+)
