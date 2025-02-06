0891b85adc8035067f410712a73345a2e1dd740c
Prabesh1sharma <sharmaprabesh2027@gmail.com>
2025-02-06 13:03:30 +0545

    data validation completed

--- src/constants/__init__.py
+++ src/constants/__init__.py
@@ -12,6 +12,7 @@ ARTIFACT_DIR: str = "artifact"
 FILE_NAME: str = "data.csv"
 TRAIN_FILE_NAME: str = "train.csv"
 TEST_FILE_NAME: str = "test.csv"
+SCHEMA_FILE_PATH = os.path.join("config","schema.yaml")
 
 
 """
@@ -22,3 +23,9 @@ DATA_INGESTION_FEATURE_STORE_DIR: str = "feature_store"
 DATA_INGESTION_INGESTED_DIR: str = "ingested"
 DATA_INGESTION_TRAIN_TEST_SPLIT_RATIO: float = 0.25
 DATA_INGESTION_COLLECTION_NAME: str = "Vechile_proj_data"
+
+"""
+Data Validation realted contant start with DATA_VALIDATION VAR NAME
+"""
+DATA_VALIDATION_DIR_NAME: str = "data_validation"
+DATA_VALIDATION_REPORT_FILE_NAME: str = "report.yaml"
