63ec51c2f06941ff11ece48f3d8210e42ac9ed9a
Prabesh1sharma <sharmaprabesh2027@gmail.com>
2025-02-07 08:33:34 +0545

    model trainer completed

--- src/constants/__init__.py
+++ src/constants/__init__.py
@@ -8,6 +8,7 @@ MONGODB_URL_KEY = "MONGODB_URL"
 
 PIPELINE_NAME: str = ""
 ARTIFACT_DIR: str = "artifact"
+MODEL_FILE_NAME:str = "model.pkl"
 
 FILE_NAME: str = "data.csv"
 TRAIN_FILE_NAME: str = "train.csv"
@@ -40,3 +41,18 @@ Data Transformation ralated constant start with DATA_TRANSFORMATION VAR NAME
 DATA_TRANSFORMATION_DIR_NAME: str = "data_transformation"
 DATA_TRANSFORMATION_TRANSFORMED_DATA_DIR: str = "transformed"
 DATA_TRANSFORMATION_TRANSFORMED_OBJECT_DIR: str = "transformed_object"
+
+"""
+MODEL TRAINER related constant start with MODEL_TRAINER var name
+"""
+MODEL_TRAINER_DIR_NAME: str = "model_trainer"
+MODEL_TRAINER_TRAINED_MODEL_DIR: str = "trained_model"
+MODEL_TRAINER_TRAINED_MODEL_NAME: str = "model.pkl"
+MODEL_TRAINER_EXPECTED_SCORE: float = 0.6
+MODEL_TRAINER_N_ESTIMATOR: int = 200
+MODEL_TRAINER_MIN_SAMPLES_SPLIT: int = 7
+MODEL_TRAINER_MIN_SAMPLES_LEAF: int = 6
+MIN_SAMPLES_SPLIT_MAX_DEPTH: int = 10
+MIN_SAMPLES_SPLIT_CRITERION: str = 'entropy'
+MIN_SAMPLES_SPLIT_RANDOM_STATE: int = 101
+MODEL_TRAINER_MODEL_CONFIG_FILE_PATH: str = os.path.join("config", "model.yaml")
